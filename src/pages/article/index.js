31c05a1550284b6e3feea1409c4f1534a9bd7ce6
Ivan Trutnev <ivan.trutneu@visata.org>
2020-02-17 18:29:31 +0300

    add editing article

--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -1,48 +1,92 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import { Link } from 'react-router-dom';
 import Loading from "../../components/Loading";
 import ErrorMessage from "../../components/ErrorMessage";
 import TagList from "../../components/TagList";
+import { CurrentUserContext } from "../../context/currentUser";
+import { Redirect } from 'react-router-dom';
 
 export const Article = (props) => {
   const slug = props.match.params.slug;
   const apiUrl = `/articles/${slug}`;
-  const [{response, error, isLoading}, doFetch] = useFetch(apiUrl);
+  const [currentUserState] = useContext(CurrentUserContext);
+  const [{response: fetchArticleResponse, error: fetchArticleError, isLoading: fetchArticleLoading}, doFetch] = useFetch(apiUrl);
+  const [{response: deleteArticleResponse}, doDeleteArticle] = useFetch(apiUrl);
+  const [isSuccessFullDelete, setIsSuccessFullDelete] = useState(false);
+
+  const isAuthor = () => {
+    if (!fetchArticleResponse || !currentUserState.isLoggedIn) {
+      return false;
+    }
+
+    return fetchArticleResponse.article.author.username === currentUserState.currentUser.username;
+  };
 
   useEffect(() => {
     doFetch();
-  }, [doFetch])
+  }, [doFetch]);
+
+  useEffect(() => {
+    if (!deleteArticleResponse) {
+      return;
+    }
+    setIsSuccessFullDelete(true);
+
+  }, [deleteArticleResponse]);
+
+  const deleteArticle = () => {
+    doDeleteArticle({
+      method: 'delete'
+    })
+  };
+
+  if (isSuccessFullDelete) {
+    return <Redirect to={`/`}/>
+  }
 
   return <div className="article-page">
     <div className="banner">
-      {!isLoading && response && (
+      {!fetchArticleLoading && fetchArticleResponse && (
         <div className="container">
-          <h1>{response.article.title}</h1>
+          <h1>{fetchArticleResponse.article.title}</h1>
           <div className="article-meta">
-            <Link to={`/profiles/${response.article.author.username}`}>
-              <img src={response.article.author.image} alt=""/>
+            <Link to={`/profiles/${fetchArticleResponse.article.author.username}`}>
+              <img src={fetchArticleResponse.article.author.image} alt=""/>
             </Link>
             <div className="info">
-              <Link to={`/profiles/${response.article.author.username}`}>
-                {response.article.author.username}
+              <Link to={`/profiles/${fetchArticleResponse.article.author.username}`}>
+                {fetchArticleResponse.article.author.username}
               </Link>
-              <span className="date">{response.article.createdAt}</span>
+              <span className="date">{fetchArticleResponse.article.createdAt}</span>
             </div>
+            {isAuthor() && (
+              <span>
+                <Link className="btn btn-outline-secondary btn-sm"
+                      to={`/articles/${fetchArticleResponse.article.slug}/edit`}>
+                  <i className="ion-edit"/>
+                  Edit article
+                </Link>
+                <button onClick={deleteArticle} className="btn btn-outline-danger btn-sm">
+                  <i className="ion-trash-a"/>
+                  Delete article
+                </button>
+              </span>
+            )}
           </div>
         </div>
       )}
     </div>
     <div className="container page">
-      {isLoading && <Loading/>}
-      {error && <ErrorMessage/>}
-      {!isLoading && response && (
+      {fetchArticleLoading && <Loading/>}
+      {fetchArticleError && <ErrorMessage/>}
+      {!fetchArticleLoading && fetchArticleResponse && (
         <div className="row article-content">
           <div className="col-xs-12">
             <div>
-              <p>{response.article.body}</p>
+              <p>{fetchArticleResponse.article.body}</p>
             </div>
-            <TagList tags={response.article.tagList}/>
+            <TagList tags={fetchArticleResponse.article.tagList}/>
           </div>
         </div>
       )}
